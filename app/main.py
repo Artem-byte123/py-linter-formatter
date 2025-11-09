17d9c405b2a7f4995cbe823ffbe92cf90e97d4ed
Artem Kurhan <artemkurhan341@gmail.com>
2025-11-09 15:12:17 +0100

    Made a dict full filed with mistakes

--- app/main.py
+++ app/main.py
@@ -1,13 +1,27 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    my_dict = {
+        "line": error.get("line_number", 0),
+        "column": error.get("column_number", 0),
+        "message": error.get("text", 0),
+        "name": error.get("code", 0),
+        "source": "flake8",
+    }
+    return my_dict
 
 
-def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+
+
+def format_single_linter_file(file_path: str, errors: list[dict]) -> dict:
+    n_dict = {
+        "errors": [format_linter_error(n) for n in errors],
+        "path": file_path,
+        "status": "passed" if not errors else "failed",
+    }
+    return n_dict
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        format_single_linter_file(path, errors)
+        for path, errors in linter_report.items()
+    ]
